417174493fa1fdebb96529395e1980fd4b8f19e3
neavemj <matthewjneave1@gmail.com>
2015-07-01 17:03:23 +0300

    added argparse and better comparison func

--- printAlignSimilarities.py
+++ printAlignSimilarities.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python
 
 # get percent similarity between core alignments
+# alignment is normal file produced by muscle
+## could turn this into a triangle comparison ##
 
-import sys
-from Bio import AlignIO
+import argparse
+from Bio import AlignIO         # requires biopython
 
-align_file = AlignIO.read(sys.argv[1], "fasta")
+
+parser = argparse.ArgumentParser("print the percent similarity between all alignments in an alignment file")
+
+parser.add_argument("--alignment_file", "-a", type = str,
+        nargs = 1, help = "alignment file as produced by Muscle")
+
+args = parser.parse_args()
+
+align_file = AlignIO.read(args.alignment_file[0], "fasta")
+
 
 def findDiffs(first, second):
-    A=list(first)
-    B=list(second)
-    count=0
-    gaps=0
+    # this function does the base by base comparison
+    A = list(first)
+    B = list(second)
+    count = 0
+    gaps = 0
     for n in range(0, len(A)):
-        if A[n]==B[n]:
-            if A[n]!="-":
-                count=count+1
+        if A[n] == B[n]:
+            if A[n] != "-" and A[n] != " ":
+                count += 1
             else:
-                gaps=gaps+1
+                gaps += 1
 
-    return 100*(count/float((len(A)-gaps)))
+    return (count / float((len(A) - gaps))) * 100
 
 def allVsAll(align):
+    # compare all alignments against all others (all-vs-all)
     for first in align:
         print "\n"
         for second in align:
